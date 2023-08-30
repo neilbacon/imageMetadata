6f65cbb11ce9028e1458d14222cdbdbb1b229cbe
Neil Bacon <2782001+neilbacon@users.noreply.github.com>
2023-08-30 16:56:32 +1000

    exif copy

---
+++ sh/exifCopy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# Usage:
+# cd photoDir
+# exifCopy .NEF v2.JPG
+
+input=$1
+output=$2
+shift 2
+
+set -x
+
+for i in *$input; do
+  o=${i%$input}$output
+  exiftool -TagsFromFile $i $o
+done
